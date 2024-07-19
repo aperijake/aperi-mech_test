dbed344d05ff69ca4508dabd2492ecee91659378
Jake Koester <jake.koester@apericmc.com>
2024-07-19 01:16:21 +0000

    add options to filter which performance tests are run

--- run_performance_tests.py
+++ run_performance_tests.py
@@ -26,7 +26,7 @@ def get_inputs_from_yaml_node(yaml_node, test_name_prefix, build_dir):
 
     return inputs
 
-def run_performance_tests_from_directory(root_dir, build_dir):
+def run_performance_tests_from_directory(root_dir, build_dir, gpu_only=False, cpu_only=False, cpu_procs=None, skip_csv=False, update_baseline=False):
     passing_tests = 0
     total_tests = 0
     
@@ -43,6 +43,15 @@ def run_performance_tests_from_directory(root_dir, build_dir):
                 yaml_node = yaml.safe_load(file)
                 test_configs = yaml_node['tests']
                 for test_config in test_configs:
+                    if test_config['hardware'] == 'gpu' and cpu_only:
+                        print(f"  Skipping test {test_config['hardware']}_{test_config['num_processors']}. --cpu set")
+                        continue
+                    if test_config['hardware'] == 'cpu' and gpu_only:
+                        print(f"  Skipping test {test_config['hardware']}_{test_config['num_processors']}. --gpu set")
+                        continue
+                    if cpu_procs and not(int(cpu_procs) == int(test_config['num_processors'])):
+                        print(f"  Skipping test {test_config['hardware']}_{test_config['num_processors']}. Request only tests with {cpu_procs} processors.")
+                        continue
                     print(f"  Running test {test_config['hardware']}_{test_config['num_processors']}")
                     inputs = get_inputs_from_yaml_node(test_config, os.path.basename(dirpath), build_dir)
                     # Command:
@@ -52,10 +61,14 @@ def run_performance_tests_from_directory(root_dir, build_dir):
                                '--time-tolerance', str(inputs['runtime_tolerance_percent']),
                                '--memory-tolerance', str(inputs['memory_tolerance_percent']),
                                '--no-plot',
-                               '--csv',
-                               '--no-ask',
-                               inputs['executable_path'],
-                               inputs['input_file']]
+                               '--no-ask']
+                    if not skip_csv:
+                        command.append('--csv')
+                    if update_baseline:
+                        command.append('--update-baseline')
+                    command.append(inputs['executable_path'])
+                    command.append(inputs['input_file'])
+
                     # Run the command
                     return_code = subprocess.call(command)
                     if return_code == 0:
@@ -78,19 +91,25 @@ def clean_logs(root_dir):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Run regression tests.')
-    parser.add_argument('--directory', help='Directory root containing the tests. Will recursively search for test.yaml files.', default='.')
+    parser.add_argument('-d', '--dir', nargs='+', help='Directory root containing the tests. Will recursively search for test.yaml files.', default='.')
     parser.add_argument('--build_dir', help='Directory containing the build', default='/home/azureuser/projects/aperi-mech/build/')
     parser.add_argument('--clean_logs', help='Clean logs in the directory', action='store_true')
+    parser.add_argument('--gpu', help='Only run GPU tests', action='store_true')
+    parser.add_argument('--cpu', help='Only run CPU tests', action='store_true')
+    parser.add_argument('--cpu_num_procs', help='Only run CPU tests with this number of processors', default=None)
+    parser.add_argument('--skip_csv', help='Skip putting reuslts in the csv file.', action='store_true')
+    parser.add_argument('--update_baseline', help='Update the baseline results.', action='store_true')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    directory = os.path.abspath(args.directory)
+    directories = [os.path.abspath(d) for d in args.dir]
 
     # Just clean the logs and exit
     if (args.clean_logs):
-        clean_logs(directory)
+        for directory in directories:
+            clean_logs(directory)
         sys.exit(0)
 
     # full path to the build directory
@@ -98,7 +117,8 @@ if __name__ == "__main__":
 
     # time the regression tests
     start_time = time.perf_counter()
-    passing_tests, total_tests = run_performance_tests_from_directory(directory, build_dir)
+    for directory in directories:
+        passing_tests, total_tests = run_performance_tests_from_directory(directory, build_dir, args.gpu, args.cpu, args.cpu_num_procs, args.skip_csv, args.update_baseline)
     end_time = time.perf_counter()
     print(f"Total time: {end_time - start_time:.4e} seconds")
 
